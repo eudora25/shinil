b7da4d7d89bafcb215320e0954e8e6cce519e676
hoony <hoony@example.com>
2025-09-01 13:15:55 +0900

    PrimeVue 의존성 제거: 빌드 에러 수정

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config'
-import 'primevue/resources/themes/lara-light-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css'
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
 
 app.mount('#app')
